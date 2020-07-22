cff0ebc11c2d512ff952c85c196118a97137b609
Christopher Eriksen <eriksenc@oregonstate.edu>
2020-07-22 15:52:10 -0700

    minor

---
+++ experiment_files/input/test_runs2.py
@@ -0,0 +1,201 @@
+import numpy as np
+import random
+import copy
+import math
+import os.path
+import csv
+import networkx as nx  
+from utils import load_params, read_graph_from_file
+from td_op import Graph
+from world_generation import generate_graph
+from schedule_generation import generate_window_base_availability_models_with_bernoulli_variance, sample_model_parameters, generate_schedule, save_base_models_to_file, save_schedules_to_file, load_base_models_from_file, load_schedules_from_file
+from plan_execution_simulation import plan_and_execute
+from planners import visualize_path_willow
+
+
+### High level code for running stat runs of task planning and simulated execution
+def stat_runs(world_config_file, schedule_config_file, planner_config_file, base_model_filepath, schedule_filepath, output_file, strategies, num_deliveries_runs, availability_percents, num_stat_runs, visualize, visualize_path):
+
+    if output_file == None:
+        record_output = False
+    else:
+        record_output = True
+
+    ## params
+    params = load_params(world_config_file, schedule_config_file, planner_config_file)
+
+    ## load world
+    # g = Graph()
+    # g.read_graph_from_file(os.path.dirname(os.path.abspath(__file__)) + params['graph_filename'])
+
+    # g = read_graph_from_file(os.path.dirname(os.path.abspath(__file__)) + params['graph_filename'])
+    g = generate_graph(params['graph_generator_type'], os.path.dirname(os.path.abspath(__file__)), params['graph_filename'], params['max_rooms'], params['max_traversal_cost'])
+
+    
+    for num_deliveries in num_deliveries_runs:
+        for availability_percent in availability_percents:
+
+            # temporal consistency parameter
+            if params['availabilities'] == 'windows':
+                available_time = params['budget']*availability_percent
+                num_windows = max(int(round(float(available_time)/params['availability_length'])), 1)
+                ave_window_offset = float(params['budget'] - available_time)/num_windows
+                mu = max(ave_window_offset, 1)
+            else:
+                mu = 60
+
+
+            # base models, true schedules
+            node_requests = []
+            base_availability_models = []
+            base_model_variances = []
+            true_availability_models = []
+            true_schedules = []
+            for stat_run in range(num_stat_runs):
+                model_file_exists = os.path.exists(base_model_filepath  + str(num_deliveries) + "_" + str(availability_percent) + "_" + str(stat_run) + ".yaml")
+                schedule_file_exists = os.path.exists(schedule_filepath + str(num_deliveries) + "_" + str(availability_percent) + "_" + str(stat_run) + ".yaml")
+                if model_file_exists and schedule_file_exists:
+                    # load pre-generated schedules/models
+                    base_avails, base_variances, requests = load_base_models_from_file(base_model_filepath, num_deliveries, availability_percent, stat_run)
+                    true_avails, schedules = load_schedules_from_file(schedule_filepath, num_deliveries, availability_percent, stat_run)
+                    node_requests.append(requests)
+                    base_availability_models.append(base_avails)
+                    base_model_variances.append(base_variances)
+                    true_availability_models.append(true_avails)
+                    true_schedules.append(schedules)
+                else:
+
+                    if params['availabilities'] == 'windows': 
+
+                        # sample rooms for delivieries 
+                        if params['node_closeness'] == 'random':
+                            node_requests.append(random.sample(params['rooms'], num_deliveries))
+                        if params['node_closeness'] == 'sequential':
+                            node_requests.append(params['rooms'][0:num_deliveries])
+                        
+                        ## base availability models
+                        avails, variances = generate_window_base_availability_models_with_bernoulli_variance(node_requests[stat_run], params['start_time'], availability_percent, params['budget'], params['time_interval'], params['availability_length'], params['availability_chance'])
+                        base_availability_models.append(avails)
+                        base_model_variances.append(variances)
+                            
+                        ## true availability models
+                        sampled_avails = sample_model_parameters(node_requests[stat_run], avails, variances, params['sampling_method'])
+                        true_availability_models.append(sampled_avails)
+
+                        ## true schedules
+                        true_schedules.append(generate_schedule(node_requests[stat_run], sampled_avails, mu, params['num_intervals'], params['schedule_generation_method']))
+                        # true_schedules.append(sample_schedule_from_model(node_requests[stat_run], sampled_avails, mu, params['num_intervals'], params['temporal_consistency']))
+
+                        save_base_models_to_file(base_model_filepath, base_availability_models[stat_run], base_model_variances[stat_run], node_requests[stat_run], num_deliveries, availability_percent, stat_run)
+                        save_schedules_to_file(schedule_filepath, true_availability_models[stat_run], true_schedules[stat_run], node_requests[stat_run], num_deliveries, availability_percent, stat_run)
+                    
+                    elif params['availabilities'] == 'simple':
+                        
+                        # sample rooms for delivieries 
+                        if params['node_closeness'] == 'random':
+                            node_requests.append(random.sample(params['rooms'], num_deliveries))
+                        if params['node_closeness'] == 'sequential':
+                            node_requests.append(params['rooms'][0:num_deliveries])
+                        
+                        ## base availability models
+                        avails, variances = generate_simple_models(node_requests[stat_run], params['start_time'], availability_percent, params['budget'], params['time_interval'], params['availability_length'], params['availability_chance'])
+                        base_availability_models.append(avails)
+                        base_model_variances.append(variances)
+                            
+                        # ## true availability models
+                        # sampled_avails = sample_model_parameters(node_requests[stat_run], avails, variances, params['sampling_method'])
+                        # true_availability_models.append(sampled_avails)
+
+                        ## true schedules
+                        true_schedules.append(generate_simple_schedules(node_requests[stat_run], sampled_avails, mu, params['num_intervals'], params['schedule_generation_method']))
+                        # true_schedules.append(sample_schedule_from_model(node_requests[stat_run], sampled_avails, mu, params['num_intervals'], params['temporal_consistency']))
+
+                        # save_base_models_to_file(base_model_filepath, base_availability_models[stat_run], base_model_variances[stat_run], node_requests[stat_run], num_deliveries, availability_percent, stat_run)
+                        # save_schedules_to_file(schedule_filepath, true_availability_models[stat_run], true_schedules[stat_run], node_requests[stat_run], num_deliveries, availability_percent, stat_run)
+                    
+                    else:
+                        raise ValueError(params['availabilities'])
+
+            ## "learned" availability models
+            availability_models = base_availability_models
+            model_variances = base_model_variances
+
+
+            # plan and execute paths for specified strategies
+            for strategy in strategies:
+                for stat_run in range(num_stat_runs):
+                    total_profit, competitive_ratio, maintenance_competitive_ratio, path_history = plan_and_execute(strategy, g, availability_models[stat_run], model_variances[stat_run], true_schedules[stat_run], node_requests[stat_run], mu, params, visualize, visualize_path)
+                    
+                    if record_output:
+                        with open(output_file, 'a', newline='') as csvfile:
+                            writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                            writer.writerow([strategy, params['budget'], num_deliveries, availability_percent, params['availability_chance'], params['maintenance_reward'], params['max_noise_amplitude'], params['variance_bias'], competitive_ratio, maintenance_competitive_ratio])
+
+
+
+
+### High level function for visualizing simulated execution
+def vizualize_sample_execution(world_config_file, schedule_config_file, planner_config_file, base_model_filepath, schedule_filepath, strategies, num_deliveries_runs, availability_percents, stat_run, visualize, visualize_path):
+
+    ## params
+    params = load_params(world_config_file, schedule_config_file, planner_config_file)
+    
+
+    ## import world
+    # g = Graph()
+    # g.read_graph_from_file(os.path.dirname(os.path.abspath(__file__)) + params['graph_filename'])
+
+    g = read_graph_from_file(os.path.dirname(os.path.abspath(__file__)) + params['graph_filename'])
+
+    for num_deliveries in num_deliveries_runs:
+        for availability_percent in availability_percents:
+
+            # temporal consistency parameter
+            if params['availabilities'] == 'windows':
+                available_time = params['budget']*availability_percent
+                num_windows = max(int(round(float(available_time)/params['availability_length'])), 1)
+                ave_window_offset = float(params['budget'] - available_time)/num_windows
+                mu = max(ave_window_offset, 1)
+            else:
+                mu = 60
+
+
+            # base models, true schedules
+            model_file_exists = os.path.exists(base_model_filepath  + str(num_deliveries) + "_" + str(availability_percent) + "_" + str(stat_run) + ".yaml")
+            schedule_file_exists = os.path.exists(schedule_filepath + str(num_deliveries) + "_" + str(availability_percent) + "_" + str(stat_run) + ".yaml")
+            if model_file_exists and schedule_file_exists:
+                # load pre-generated schedules/models
+                base_availability_models, base_model_variances, node_requests = load_base_models_from_file(base_model_filepath, num_deliveries, availability_percent, stat_run)
+                true_availability_models, true_schedules = load_schedules_from_file(schedule_filepath, num_deliveries, availability_percent, stat_run)
+            else:
+                # sample rooms for delivieries 
+                if params['node_closeness'] == 'random':
+                    node_requests = random.sample(params['rooms'], num_deliveries)
+                if params['node_closeness'] == 'sequential':
+                    node_requests = params['rooms'][0:num_deliveries]
+                
+                ## base availability models
+                base_availability_models, base_model_variances = generate_window_base_availability_models_with_bernoulli_variance(node_requests, params['start_time'], availability_percent, params['budget'], params['time_interval'], params['availability_length'], params['availability_chance'])
+                    
+                ## true availability models
+                true_availability_models = sample_model_parameters(node_requests, base_availability_models, base_model_variances, params['sampling_method'])
+
+                ## true schedules
+                true_schedules = generate_schedule(node_requests, true_availability_models, mu, params['num_intervals'], params['schedule_generation_method'], params['temporal_consistency'])
+
+                save_base_models_to_file(base_model_filepath, base_availability_models, base_model_variances, node_requests, num_deliveries, availability_percent, stat_run)
+                save_schedules_to_file(schedule_filepath, true_availability_models, true_schedules, node_requests, num_deliveries, availability_percent, stat_run)
+                    
+
+            ## "learned" availability models
+            availability_models = base_availability_models
+            model_variances = base_model_variances
+
+
+            # plan and execute paths for specified strategies
+            visit_traces = {}
+            for strategy in strategies:
+                total_profit, competitive_ratio, maintenance_competitive_ratio, path_history = plan_and_execute(strategy, g, availability_models, model_variances, true_schedules, node_requests, mu, params, visualize, visualize_path)
+                visit_traces[strategy] = path_history
+
+            visualize_path_willow(strategies, visit_traces, true_availability_models, true_schedules, node_requests, params['maintenance_node'], params['start_time'], params['budget'], params['time_interval'])
---
+++ experiment_files/input/untitled.py
@@ -0,0 +1,255 @@
+import numpy as np
+import random
+import copy
+import math
+import yaml
+
+def bernoulli_variance(availability_prob):
+    return availability_prob*(1-availability_prob)
+
+def sample_bernoulli_avialability_model(availability_model):
+    avails = []
+    for avail in availability_model:
+        new_avail = max(min(random.gauss(avail, math.sqrt(bernoulli_variance(avail))), 0.99), 0.01)
+        avails.append(new_avail)
+    return avails
+
+def sample_occupancy(prob):
+    if random.random() < prob:
+        return 1
+    else:
+        return 0
+
+def threshold_occupancy(prob):
+    threshold = .5
+    if prob > threshold:
+        return 1
+    else:
+        return 0
+
+### Temporal persistence per Toris, Russell, and Sonia Chernova. "Temporal Persistence Modeling for Object Search." IEEE International Conference on Robotics and Automation (ICRA). 2017.
+def persistence_prob(mu, delta_t, last_observation):
+    if last_observation == 1:
+        return math.exp(-(1.0/mu)*(delta_t))
+    else:
+        return 1.0 - math.exp(-(1.0/mu)*(delta_t))
+
+
+def generate_simple_models():
+
+
+def generate_windows(node_requests, start_time, availability_percent, budget, time_interval, availability_length, availability_chance):
+
+    # generate base availability model with corresponding variance
+    base_availability_models = {}
+    model_variances = {}
+    for request in node_requests:   
+
+        available_time = budget*availability_percent
+        num_windows = max(1, int(round(float(available_time)/availability_length)))
+        new_availability_length = int(float(available_time)/num_windows)
+        ave_window_offset = float(budget - available_time)/num_windows
+
+        max_shift = ave_window_offset*.1
+        max_additional_spread = new_availability_length*.1
+
+        initial_shift = int(start_time + random.random()*ave_window_offset/2.0)
+        window_high = min(int(initial_shift + new_availability_length + random.random()*2*max_additional_spread - max_additional_spread), start_time + budget)
+        old_window_high = window_high
+        windows = [[initial_shift, window_high]]
+        for window in range(num_windows):
+            window_low = max(start_time, int(old_window_high + ave_window_offset + random.random()*2*max_shift - max_shift))
+            window_high = min(int(window_low + new_availability_length + random.random()*2*max_additional_spread - max_additional_spread), start_time + budget)
+            old_window_high = window_high
+            windows.append([window_low, window_high])
+        def window_check(x, windows):
+            available = 1.0 - availability_chance
+            for window in windows:
+                if (window_low <= x <= window_high):
+                    available = availability_chance
+            return available
+        
+        avails = []
+        variances = []
+        t = start_time
+        num_intervals = int((budget - start_time)/time_interval)
+        for i in range(num_intervals):
+            avail = window_check(t, windows)
+            avails.append(avail)
+            variances.append(bernoulli_variance(avail))
+            t += time_interval
+        base_availability_models[request] = avails
+        model_variances[request] = variances
+
+    return base_availability_models, model_variances
+
+
+### Generate base availability models with corresponding Bernoulli variance. To be used as planner models or sampled to produce "true" simulator model.
+def generate_window_base_availability_models_with_bernoulli_variance(node_requests, start_time, availability_percent, budget, time_interval, availability_length, availability_chance):
+
+    # generate base availability model with corresponding variance
+    base_availability_models = {}
+    model_variances = {}
+    for request in node_requests:   
+
+        available_time = budget*availability_percent
+        num_windows = max(1, int(round(float(available_time)/availability_length)))
+        ave_window_offset = float(budget - available_time)/num_windows
+
+        max_shift = ave_window_offset*.1
+        max_additional_spread = availability_length*.1
+
+        initial_shift = int(start_time + random.random()*availability_length/2.0)
+        window_high = min(int(initial_shift + availability_length + random.random()*2*max_additional_spread - max_additional_spread), start_time + budget)
+        old_window_high = window_high
+        windows = [[initial_shift, window_high]]
+        for window in range(num_windows):
+            window_low = max(start_time, int(old_window_high + random.random()*2*max_shift - max_shift))
+            window_high = min(int(window_low + availability_length + random.random()*2*max_additional_spread - max_additional_spread), start_time + budget)
+            old_window_high = window_high
+            windows.append([window_low, window_high])
+        def window_check(x, windows):
+            available = 1.0 - availability_chance
+            for window in windows:
+                if (window_low <= x <= window_high):
+                    available = availability_chance
+            return available
+        
+        avails = []
+        variances = []
+        t = start_time
+        num_intervals = int((budget - start_time)/time_interval)
+        for i in range(num_intervals):
+            avail = window_check(t, windows)
+            avails.append(avail)
+            variances.append(bernoulli_variance(avail))
+            t += time_interval
+        base_availability_models[request] = avails
+        model_variances[request] = variances
+
+    return base_availability_models, model_variances
+
+
+### Sample individual model from base model with variance.
+def sample_model_parameters(node_requests, base_availability_models, model_variances, sampling_method='gauss'):
+    sampled_availability_models = {}
+    for request in node_requests:
+        new_avails = []
+        for i in range(len(base_availability_models[request])):
+            old_avail = base_availability_models[request][i]
+            variance = model_variances[request][i]
+
+            if sampling_method == 'gauss':
+                new_avail = max(min(random.gauss(old_avail, math.sqrt(variance)), 0.99), 0.01)
+            new_avails.append(new_avail)
+
+        sampled_availability_models[request] = new_avails
+
+    return sampled_availability_models
+
+
+
+### Generate schedule from "true" model
+def generate_schedule(node_requests, availability_models, mu, num_intervals, schedule_generation_method, incorporate_observation):
+    if schedule_generation_method == 'threshold':
+        return generate_schedule_from_model_windows(node_requests, availability_models, mu, num_intervals)
+    elif schedule_generation_method == 'temporal_consistency':
+        return sample_schedule_from_model_temporal_consistency(node_requests, availability_models, mu, num_intervals, incorporate_observation)
+    else:
+        raise ValueError(schedule_generation_method)
+
+### Threshold simulated availabilities given "true" model
+def generate_schedule_from_model_windows(node_requests, availability_models, mu, num_intervals):
+    schedules = {}
+    for request in node_requests:
+        availability_model = availability_models[request]
+        occupancies = []
+        for i in range(num_intervals):
+            occ = threshold_occupancy(availability_model[i])
+            occupancies.append(occ)
+        schedules[request] = occupancies
+    return schedules
+
+### Sample simulated availabilities given "true" model, incorporating temporal consistency
+def sample_schedule_from_model_temporal_consistency(node_requests, availability_models, mu, num_intervals, temporal_consistency=True):
+    schedules = {}
+    for request in node_requests:
+        availability_model = availability_models[request]
+        initial_occ = sample_occupancy(availability_model[0])
+        prev_occ = initial_occ
+        occupancies = [initial_occ]
+        for i in range(num_intervals)[1:]:
+            if temporal_consistency:
+                likelihood = persistence_prob(mu, 1, prev_occ)
+                # if prev_occ == 1:
+                #     evidence_prob = availability_model[i]
+                # else:
+                #     evidence_prob = 1 - availability_model[i]
+                evidence_prob = likelihood*availability_model[i] + (1.0-likelihood)*(1.0-availability_model[i])
+                new_prob = likelihood*availability_model[i]/evidence_prob         # Bayesian update of last observation times occ prior
+                occ = sample_occupancy(new_prob)
+                occupancies.append(occ)
+                prev_occ = occ
+            else:
+                occ = sample_occupancy(availability_model[i])
+                occupancies.append(occ)
+
+        schedules[request] = occupancies
+
+    return schedules
+
+
+
+def load_base_models_from_file(base_model_filepath, num_deliveries, availability_percent, stat_run):
+    filename = base_model_filepath  + str(num_deliveries) + "_" + str(availability_percent) + "_" + str(stat_run) + ".yaml"
+    with open(filename) as f:
+        in_dict = yaml.load(f, Loader=yaml.FullLoader)
+    node_requests = in_dict['node_requests']
+    base_availability_models = {}
+    base_model_variances = {}
+    for node_request in node_requests:
+        avails = []
+        variances = []
+        for i in range(len(in_dict['base_availability_models'][node_request])):
+            avails.append(float(in_dict['base_availability_models'][node_request][i]))
+            variances.append(float(in_dict['base_model_variances'][node_request][i]))
+        base_availability_models[node_request] = avails
+        base_model_variances[node_request] = variances              
+    return base_availability_models, base_model_variances, node_requests
+
+def load_schedules_from_file(schedule_filepath, num_deliveries, availability_percent, stat_run):
+    filename = schedule_filepath + str(num_deliveries) + "_" + str(availability_percent) + "_" + str(stat_run) + ".yaml"
+    with open(filename) as f:
+        in_dict = yaml.load(f, Loader=yaml.FullLoader)
+    node_requests = in_dict['node_requests']
+    true_availability_models = {}
+    true_schedules = {}
+    for node_request in node_requests:
+        avails = []
+        schedules = []
+        for i in range(len(in_dict['true_availability_models'][node_request])):
+            avails.append(float(in_dict['true_availability_models'][node_request][i]))
+            schedules.append(int(in_dict['true_schedules'][node_request][i]))
+        true_availability_models[node_request] = avails
+        true_schedules[node_request] = schedules    
+
+    return true_availability_models, true_schedules
+    
+
+def save_base_models_to_file(base_model_filepath, base_availability_models, base_model_variances, node_requests, num_deliveries, availability_percent, stat_run):
+    filename = base_model_filepath + str(num_deliveries) + "_" + str(availability_percent) + "_" + str(stat_run) + ".yaml"
+    out_dict = {}
+    out_dict['base_availability_models'] = base_availability_models
+    out_dict['base_model_variances'] = base_model_variances
+    out_dict['node_requests'] = node_requests
+    with open(filename, 'w') as f:
+         yaml.dump(out_dict, f)
+
+def save_schedules_to_file(schedule_filepath, true_availability_models, true_schedules, node_requests, num_deliveries, availability_percent, stat_run):
+    filename = schedule_filepath + str(num_deliveries) + "_" + str(availability_percent) + "_" + str(stat_run) + ".yaml"
+    out_dict = {}
+    out_dict['true_availability_models'] = true_availability_models
+    out_dict['true_schedules'] = true_schedules
+    out_dict['node_requests'] = node_requests
+    with open(filename, 'w') as f:
+        yaml.dump(out_dict, f)
