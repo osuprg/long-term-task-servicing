528d6aa117b0e0f4dbdb4bedd13405c2daebe080
Christopher Eriksen <eriksenc@oregonstate.edu>
2020-08-11 16:13:34 -0700

    minor

--- test_runs.py
+++ test_runs.py
@@ -94,12 +94,12 @@ def stat_runs(world_config_file, schedule_config_file, planner_config_file, mode
 
 
                         # true schedule
-                        schedules = {}
-                        for request in node_requests[stat_run]:
-                            x_in, y_in = load_brayford_testing_data(request, os.path.dirname(os.path.abspath(__file__)) + params['data_path'], stat_run)
-                            test_gp = GP(None, x_in, y_in, params['budget'], 1, params['noise_scaling'], True, 'values')
-                            schedules[request] = test_gp.threshold_sample_schedule(params['start_time'], params['budget'], params['time_interval'])
-                        true_schedules.append(schedules)
+                        # schedules = {}
+                        # for request in node_requests[stat_run]:
+                        #     x_in, y_in = load_brayford_testing_data(request, os.path.dirname(os.path.abspath(__file__)) + params['data_path'], stat_run)
+                        #     test_gp = GP(None, x_in, y_in, params['budget'], 1, params['noise_scaling'], True, 'values')
+                        #     schedules[request] = test_gp.threshold_sample_schedule(params['start_time'], params['budget'], params['time_interval'])
+                        # true_schedules.append(schedules)
 
 
                     elif params['availabilities'] == 'windows': 
